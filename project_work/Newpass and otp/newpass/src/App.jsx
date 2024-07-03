fa1f1a2e75b9e8a793a5303f80e637dbe46eff2d
owaiskhilji <owaiskhilji6@gmail.com>
2024-07-03 21:22:23 +0500

    newpassword and otp

---
+++ project_work/Newpass and otp/newpass/src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Newpass from './Components/Newpass/Newpass';
+import Otp from './Components/otp/Otp';
+
+function App() {
+  return (
+
+    <Newpass/>
+    // <Otp/>
+  )
+}
+
+export default App
